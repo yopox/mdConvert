43fe53f50e413255f4f910ef6df2d14ed6b4c4fb
pierrotdu18 <pierre.elis@hotmail.fr>
2016-10-28 22:22:28 +0200

    Some more documentation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Little tool to convert Markdown to cool LaTeX documents.
-# Written by YoPox and Hadrien
+# Written by YoPox, Hadrien and pierrotdu18
 
 # # # # # #
 # Import  #
@@ -14,36 +14,41 @@ import sys
 global doc
 
 doc = """
-mdConvert full version
+mdConvert help
+Full version
 
 Usage :
-    main.py <input> <options>
-    main.py --help
+    main.py <input> <options> (normal use of the function, provides a fresh .tex document)
+    main.py --help (to get this text exactly)
 
 Options :    
     -o : shortcut for --output
-    --ouput <output> : output file name
+    --ouput <output> : output file name (same as input file name by default)
 
     -d : shortcut for --date
-    --date <date> : date of the document
+    --date <date> : date of the document (none by default)
 
     -a : shortcut for --author
-    --author <author> : author of the document
+    --author <author> : author of the document (none by default)
 
     -t : shortcut for --title
-    --title <title> : title of the document
+    --title <title> : title of the document (none by default)
 
     -c : shortcut for --documentclass
-    --documentclass <class> : document class
+    --documentclass <class> : document class (article by default)
 
     -p : shortcut for --packages
-    --packages <pcks> : list of additionnal packages with syntax {[options1]{package1},[options2]{package2},...}
+    --packages <pcks> : list of additionnal packages with syntax 
+                        {[options1]{package1},[options2]{package2},...}
+                        (none by default)
 
     -T : shortcut for --tableofcontents
-    --tableofcontents : if a table of contents is needed
+    --tableofcontents : if a table of contents is needed ('on' by default)
 
-    -m : shortcut for --minted
+    -m : shortcut for --minted ('off' by default) (/!\ HAS NOT BEEN IMPLEMENTED YET)
     --minted : if the code should be colored with minted (requires Pygments and --shell-escape option to compile)
+
+Go to https://github.com/YoPox/mdConvert/ for more information
 """
 
 # # # # # # # # # # # #
